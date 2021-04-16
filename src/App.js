6fecb558ae60b04b0492a874c2d37887d1ba2754
stefzouille <contact.lokcy@gmail.com>
2021-04-16 23:01:15 +0200

    add vol2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import ClassState from "./components/ClassState";
+import FunctionState from "./components/FunctionState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1>UseState Hooks</h1>
+      <ClassState />
+      <hr /> {/* hr --> affiche une ligne de separation*/}
+      <FunctionState />
     </div>
   );
 }
---
+++ src/components/ClassState.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class ClassState extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  addOne = () => {
+    this.setState((prevState) => {
+      return {
+        counter: prevState.counter + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Class state: {this.state.counter}</p>
+
+        <button onClick={this.addOne}>State dans Class</button>
+      </div>
+    );
+  }
+}
+
+export default ClassState;
